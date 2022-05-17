1bc77ad12af3f31c056a64ac9d3689214f4fdaef
Emmanuelle Amouroux <amouroux.emmanuelle@orange.fr>
2022-05-17 14:03:43 +0200

    2

--- db/migrate/20220517084735_add_defaultvalueto_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultvaluetoTasks < ActiveRecord::Migration[7.0]
-  def change
-    change_column :task, :completed, :default => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_084408) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_090605) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "detail"
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "taskts", force: :cascade do |t|
     t.string "title"
     t.string "detail"
